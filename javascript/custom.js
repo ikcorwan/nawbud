ea94909f2d0a3e64e076119232de8649fdbcd7a5
Jarosław Nawrocki <nawrockij@o2.pl>
2020-02-28 17:09:04 +0100

    Update images and change page style (color theme)

---
+++ javascript/custom.js
@@ -0,0 +1,52 @@
+//Close mobile nav to click
+
+$(document).ready(function() {
+$(document).click(function(event){
+ 		var clickover = $(event.target);
+		var _opened = $('.navbar-collapse').hasClass("show");
+		if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+			$(".navbar-toggler").click();				
+ 		}
+ 	});
+ });
+
+
+
+//Offer - Show More - Button
+
+$(document).ready(function(){
+  $(".button__more").click(function(){
+    $(".offer__more").addClass("offer__more--is-visible");
+    $(".button__more").addClass("button__more--is-hidden");
+
+  });
+});
+
+//Counter
+
+
+$(document).ready(function() {
+	$('.counter__timer').counterUp({
+	  delay: 10,
+	  time: 1600
+	});
+	$('.counter__timer').addClass('animated fadeInDownBig');
+	$('h3').addClass('animated fadeIn');
+});
+
+//Back to Top - Button
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("button__top").style.display = "block";
+  } else {
+    document.getElementById("button__top").style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
