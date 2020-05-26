7fbd2dc9f630c5b087273eaca87b55b526b0a161
Jarosław Nawrocki <nawrockij@o2.pl>
2020-05-26 14:37:56 +0200

    Add a privacy policy, whcookies.js

---
+++ javascript/whcookies.js
@@ -0,0 +1,40 @@
+/*
+ * Skrypt wyświetlający okienko z informacją o wykorzystaniu ciasteczek (cookies)
+ * 
+ * Więcej informacji: http://webhelp.pl/artykuly/okienko-z-informacja-o-ciasteczkach-cookies/
+ * 
+ */
+
+function WHCreateCookie(name, value, days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    var expires = "; expires=" + date.toGMTString();
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+function WHReadCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+
+window.onload = WHCheckCookies;
+
+function WHCheckCookies() {
+    if(WHReadCookie('cookies_accepted') != 'T') {
+        var message_container = document.createElement('div');
+        message_container.id = 'cookies-message-container';
+        var html_code = '<div id="cookies-message" style="padding: 6px 0px; font-size: .9rem; line-height: 1.3rem; border-top: 2px solid #212121; text-align: center; position: fixed; bottom: 0px; background-color: rgba(33, 33, 33, 0.8);; width: 100%; z-index: 9999;">Ta strona używa plików Cookies (ciasteczek). <a href="privacy-policy.html">Więcej informacji</a><a href="javascript:WHCloseCookiesWindow();" id="accept-cookies-checkbox" name="accept-cookies" style="background-color:#333333; padding: 5px 10px; color: white; border-radius: 4px; -moz-border-radius: 4px; -webkit-border-radius: 4px; display: inline-block; margin-left: 10px; text-decoration: none; cursor: pointer;">Akceptuj <i class="fas fa-times" style="margin-left: 10px;"></i></a></div>';
+        message_container.innerHTML = html_code;
+        document.body.appendChild(message_container);
+    }
+}
+
+function WHCloseCookiesWindow() {
+    WHCreateCookie('cookies_accepted', 'T', 365);
+    document.getElementById('cookies-message-container').removeChild(document.getElementById('cookies-message'));
+}
