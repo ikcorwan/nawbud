8ac86c363293c1504c5972a806b8bdab561f5b1f
Jarosław Nawrocki <nawrockij@o2.pl>
2020-06-04 12:06:45 +0200

    upgrade to production version

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 
 var cssDest = 'style';
@@ -19,6 +21,22 @@ gulp.task('buildcss', function(){
         }))
         .pipe(sourcemaps.write('../maps'))
 		.pipe(gulp.dest(cssDest));
+});
+
+gulp.task('concat', function(){
+  return gulp.src([
+  	'./source/javascript/jquery-3.3.1.min.js', 
+  	'./source/javascript/bootstrap.min.js', 
+  	'./source/javascript/whcookies.js',
+  	'./source/javascript/jquery.waypoints.min.js',
+  	'./source/javascript/jquery.counterup.js',
+  	'./source/javascript/custom.js'
+
+
+  	])
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('javascript'));
 });
 
 gulp.task('watch', function(){
